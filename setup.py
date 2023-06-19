70fd1477d9887eeeb9058acfdcd1494581e23598
Sameer <sameer1_20@manavrachna.net>
2023-06-19 23:18:50 +0530

    EDA and Problems

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def get_requirements(file_path:str)->List[str]:
     requirements=[]
     with open(file_path) as file_obj:
         requirements=file_obj.readlines()
-        requirements=[req.replace("\n","")for req in requirements]
+        requirements=[req.replace("\n","") for req in requirements]
 
         if hyphen_e_dot in requirements:
             requirements.remove(hyphen_e_dot)
